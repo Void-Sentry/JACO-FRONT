12a4fb351374a5467f1e15133d5f33328cb0a381
Oséias Batista Pegado <oseias.batista.dev@gmail.com>
2022-12-11 10:36:14 -0300

    ft: direct chat

--- components/direct-chat/direct-chat.vue
+++ components/direct-chat/direct-chat.vue
@@ -10,7 +10,7 @@
         <input v-model="text" @keypress.enter="send" type="text"
             class="my-auto text-xl text-primary placeholder:text-primary focus:outline-none h-full bg-tertiary w-full mx-1.5"
             :placeholder="`Conversar em @${channelName}`" />
-        <button class="text-success my-auto mr-9 ml-auto hover:underline text-xl">
+        <button class="text-success my-auto mr-9 ml-auto hover:underline text-xl" @click.stop="send">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#385E72"
                 class="w-10 h-10">
                 <path stroke-linecap="round" stroke-linejoin="round"
